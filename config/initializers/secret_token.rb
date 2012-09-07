9b8049c8876ece3999a46c09007bfbb05c0203bc
l.john.ribar <john.ribar@gettyimages.com>
2012-09-07 15:51:52 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GypsyBlog::Application.config.secret_token = '2cd5b493bb1af0e02f909ed046ac2d2a2e87abafcb99efa1f58063f868c1ecddc8971b15effb7d59da4925fb1210f156247a75c8c0112da054fdfdf1aa0900db'
